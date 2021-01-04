53d6c45ceb57feb19e7c08bc581192d70f1b2e14
timtim1342 <tim.muha@gmail.com>
2021-01-04 18:13:48 +0300

    First commit.

---
+++ R/features_maker.R
@@ -0,0 +1,138 @@
+# packages
+library(tidyverse)
+library(lingtypology)
+library(DT)
+
+extr_map <- function(fe) {
+  vill <- read_csv("./data/villages.csv") # villages dataset
+  meta <- read_tsv("./data/meta.csv") # language metadata and colors
+  
+  # preparation of data
+  vill <- vill[complete.cases(vill$lat),] # remove villages for which we do not have coordinates (yet)
+  vill <- vill %>% 
+    filter(kutans == 'FALSE')  # убирает кутаны
+  
+  meta_core <- meta %>% # remove idioms not (yet) recognized as distinct
+    filter(core == "yes")
+  
+  vill_meta <- merge(vill, meta_core, by = "lang") # merge villages and coordinates with language metadata
+  vill_meta <- vill_meta %>% 
+    mutate(idiom = idiom.y)  # почему там два разных идиома?
+  
+   fe_vill <- merge(vill_meta, fe, by = "lang") # merge villages, coordinates, and language metadata with feature information
+   fe_vill$datapoint <- "extrapolated datapoint"
+  
+
+  mp <- map.feature(lang.gltc(fe_vill$glottocode),
+                    latitude = fe_vill$lat,
+                    longitude = fe_vill$lon,
+                    features = fe_vill$lang, # color feature = language
+                    color = fe_vill$lang_color,
+                    stroke.features = fe_vill$value, # stroke.feature = your feature value
+                    stroke.color = c("black", "white"),
+                    label = fe_vill$lang,
+                    zoom.control = T,
+                    popup = paste("<b>Village:</b>", fe_vill$village, "<br>",
+                                  "<b>Source:</b>", fe_vill$source, fe_vill$page, "<br>",
+                                  "<b>Datapoint:</b>", fe_vill$datapoint),
+                    width = 3,
+                    stroke.radius = 8,
+                    legend = FALSE)
+  
+  return(mp)
+}
+
+gen_map <- function(fe) {
+  vill <- read_csv("./data/villages.csv") # villages dataset
+  meta <- read_tsv("./data/meta.csv") # language metadata and colors
+  
+  # preparation of data
+  vill <- vill[complete.cases(vill$lat),] # remove villages for which we do not have coordinates (yet)
+  vill <- vill %>% 
+    filter(kutans == 'FALSE')  # убирает кутаны
+  
+  meta_core <- meta %>% # remove idioms not (yet) recognized as distinct
+    filter(core == "yes")
+  
+  vill_meta <- merge(vill, meta_core, by = "lang") # merge villages and coordinates with language metadata
+  vill_meta <- vill_meta %>% 
+    mutate(idiom = idiom.y)  # почему там два разных идиома?
+  
+  fe_vill <- merge(vill_meta, fe, by = "lang") # merge villages, coordinates, and language metadata with feature information
+  fe_vill$datapoint <- "extrapolated datapoint"
+  
+  # filter core languages
+  core_meta <- meta %>%
+    filter(core == "yes")
+  core_data <- left_join(core_meta, fe, by = "lang")
+  core_data$datapoint <- "general datapoint"
+  
+  # draw a map
+ mp <- map.feature(lang.gltc(core_data$glottocode),
+              latitude = core_data$gen_lat,  # gltc_lat
+              longitude = core_data$gen_lon,  # gltc_lon
+              features = core_data$lang, # color feature = language
+              color = core_data$lang_color,
+              stroke.features = core_data$value, # stroke.feature = your feature value
+              stroke.color = c("black", "white"),
+              label = core_data$lang,
+              zoom.control = T,
+              popup = paste("<b>Source:</b>", core_data$source, core_data$page, "<br>",
+                            "<b>Datapoint:</b>", core_data$datapoint),
+              width = 3, stroke.radius = 8,
+              legend = FALSE)
+ return(mp)
+}
+
+d_b <- function(fe, col_names) {
+  meta <- read_tsv("./data/meta.csv") # language metadata and colors
+  # preparation of data
+  meta <- meta %>%
+    filter(core == "yes") %>% 
+    select(lang, family, aff)
+  fe <- left_join(meta, fe, by = "lang")
+  fe <- fe %>% 
+    mutate(aff = factor(aff)) %>% 
+    mutate(family = factor(family))
+  
+  # select which feature data you want to show in the datatable
+  dtable <- fe %>%
+    select(col_names)
+  cit <- vector("character", length(dtable$source))
+  for (i in seq_along(dtable$source))
+  {
+    if (grepl("; ", dtable$source[[i]], fixed = TRUE) == TRUE)
+    {
+      cits = strsplit(dtable$source[[i]], "; ")
+      for (j in seq_along(cits))
+      {
+        cits[[j]] <- RefManageR::Cite(bib = bib, cits[[j]], .opt = list(max.names = 2))
+      }
+      dtable$source[[i]] = paste(cits, sep = "; ")
+    }
+    else
+    {
+      dtable$source[[i]] <- RefManageR::Cite(bib = bib, dtable$source[[i]], .opt = list(max.names = 2))
+    }
+  }
+  # generate searchable datatable
+  db <- DT::datatable(dtable,
+                      filter = "top",
+                      escape = FALSE,
+                      rownames = FALSE,
+                      extensions = c('Buttons', 'FixedColumns'),  # buttons
+                      options = list(
+                        pageLength = 5,  # columnDefs = list(list(searchable = FALSE, targets = 0)),
+                        paging = TRUE,
+                        searching = TRUE,
+                        fixedColumns = TRUE,
+                        ordering = TRUE,
+                        dom = 'fBltpi',  # add map-lang-link button, fff
+                        scrollX = TRUE,
+                        buttons = list(list(
+                        extend = 'collection',
+                        buttons = c('csv', 'excel', 'pdf'),
+                        text = '<i class="fas fa-download"></i>'
+                        ))))
+  return(db)
+}
